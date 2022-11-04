8c2c8f756f107889af52bb32f46f776605a90b22
Alex Márquez <115575909+Alexmarkq@users.noreply.github.com>
2022-11-04 11:10:00 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const app = {
-    appName: 'Canvas game',
+    appName: 'Amanita`s Journey',
     version: '1.0.0',
     licensed: undefined,
     author: 'Alexis Marquez & Alberto Naval',
@@ -24,18 +24,16 @@ const app = {
     init() {
         this.setDimensions()
         this.setContext()
+        this.reset()
         this.createEnemies()
         this.createTube()
         this.start()
         this.createPlatforms()
         this.player.setEventHandlers()
         this.backGroundMusic.play()
-
     },
 
     start() {
-        this.reset()
-
         this.interval = setInterval(() => {
             this.framesCounter > 5000 ? this.framesCounter = 0 : this.framesCounter++
             if (this.framesCounter % 100 === 0) this.createPlatforms()
@@ -47,8 +45,9 @@ const app = {
             this.clearPlatforms()
             this.playerEnemiesCollision()
             this.playerTubeCollision()
-            this.isGameOver()
+            this.goOut()
             this.isVictory()
+
         }, 1000 / this.FPS)
     },
 
@@ -60,8 +59,6 @@ const app = {
         document.querySelector('#myCanvas').setAttribute('width', this.canvasSize.w)
         document.querySelector('#myCanvas').setAttribute('height', this.canvasSize.h)
     },
-
-
 
     clearInterval() {
         this.ctx.clearRect(0, 0, this.canvasSize.w, this.canvasSize.h)
@@ -108,6 +105,12 @@ const app = {
         this.enemies.forEach(enemy => enemy.drawEnemies())
     },
 
+    goOut() {
+        if (this.player.playerPos.y > this.canvasSize.h + this.player.playerSize.h) {
+            this.isGameOver()
+        }
+    },
+
     playerPlatformColission() {
         this.platforms.forEach((p) => {
             if (
@@ -123,7 +126,6 @@ const app = {
                     this.playerVel *= 1
                     this.player.playerPos.x = p.platformPos.x - this.player.playerSize.w
                     console.log('izquierda')
-
                 } else {
                     this.player.playerPos.y = p.platformPos.y - this.player.playerSize.h + 10
                     this.player.canJump = true
@@ -144,12 +146,12 @@ const app = {
             ) {
                 if (e.enemiesPos.x + e.enemiesSize.w - 10 > this.player.playerPos.x &&
                     this.player.playerPos.y > e.enemiesPos.y) {
-                    this.player.playerPos.y > this.canvasSize.h
+                    //this.player.playerPos.y = this.canvasSize.h + this.player.playerSize.h
                     this.isGameOver()
                     console.log('izquierda enemigo')
                 } else if (e.enemiesPos.x + e.enemiesSize.w > this.player.playerPos.x &&
                     this.player.playerPos.y > e.enemiesPos.y) {
-                    this.player.playerPos.y > this.canvasSize.h
+                    //this.player.playerPos.y = this.canvasSize.h + this.player.playerSize.h
                     this.isGameOver()
                     console.log('derecha enemigo')
                 } else {
@@ -209,15 +211,14 @@ const app = {
         this.ctx.fillText(`YOU SUCK MOTHERFUCKER!!`, this.canvasSize.w / 2, this.canvasSize.h / 2 + 100)
         this.ctx.fillText(`You killed: ${this.score / 100} enemies`, this.canvasSize.w / 2, this.canvasSize.h / 2 + 200)
     },
+
     isGameOver() {
-        if (this.player.playerPos.y > this.canvasSize.h + this.player.playerSize.h) {
-            clearInterval(this.interval)
-            this.drawGameOver()
-            this.backGroundMusic.pause()
-            this.gameOver.play()
-            console.log('GameOver')
-            this.restartGameOver()
-        }
+        clearInterval(this.interval)
+        this.drawGameOver()
+        this.backGroundMusic.pause()
+        this.gameOver.play()
+        console.log('GameOver')
+        this.restartGameOver()
     },
 
     drawVictory() {
